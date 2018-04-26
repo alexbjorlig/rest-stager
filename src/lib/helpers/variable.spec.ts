17659f228c9706c14ca8559318c3e519cc83ccdf
Alex Bjørlig <alexbjorlig@icloud.com>
2018-04-26 21:53:08 +0200

    replacing mocha with jest! Exiting times!

--- src/lib/helpers/variable.spec.ts
+++ src/lib/helpers/variable.spec.ts
@@ -1,5 +1,4 @@
 
-import { expect } from 'chai';
 import { vCheck } from './variable';
 
 describe('Testing variables', () =>  {
@@ -7,17 +6,17 @@ describe('Testing variables', () =>  {
     describe('the vCheck function', () =>  {
         it(`vCheck should return true with the following string: /a/simple/{{v_test}}`, () =>  {
             const result = vCheck('/a/simple/{{v_test}}');
-            expect(result).to.equal(true);
+            expect(result).toEqual(true);
         });
 
         it(`vCheck should return false with the following string: '/a/simple/v_test/test'`, () =>  {
             const result = vCheck('/a/simple/v_test/test');
-            expect(result).to.equal(false);
+            expect(result).toEqual(false);
         });
 
         it(`vCheck should return false, when given an empty string`, () =>  {
             const result = vCheck('');
-            expect(result).to.equal(false);
+            expect(result).toEqual(false);
         });
     });
 
